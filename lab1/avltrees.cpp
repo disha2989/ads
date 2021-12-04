84f07b1902f9e9f29a77cfec3ff8d4323f646341
disha2989 <71320045+disha2989@users.noreply.github.com>
2021-12-04 12:46:10 +0530

    Create avltrees.cpp

---
+++ lab1/avltrees.cpp
@@ -0,0 +1,181 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// An AVL tree node
+class Node
+{
+    public:
+    int key;
+    Node *left;
+    Node *right;
+    int height;
+}; 
+
+// A utility function to get maximum
+// of two integers
+int max(int a, int b);
+ 
+// A utility function to get the
+// height of the tree
+int height(Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+
+// A utility function to get maximum
+// of two integers
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+
+/* Helper function that allocates a
+   new node with the given key and
+   NULL left and right pointers. */
+Node* newNode(int key)
+{
+    Node* node = new Node();
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1; // new node is initially
+                      // added at leaf
+    return(node);
+}
+
+// A utility function to right
+// rotate subtree rooted with y
+Node *rightRotate(Node *y)
+{
+    Node *r = y->left;
+    Node *t = r->right;
+ 
+    // Perform rotation
+    r->right = y;
+    y->left  = t;
+
+// Update heights
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+    r->height = max(height(r->left),
+                    height(r->right)) + 1;
+                    
+    // Return new root
+    return r;
+}
+
+// A utility function to left
+// rotate subtree rooted with x
+Node *leftRotate(Node *y)
+{
+    Node *r = y->right;
+    Node *t = r->left;
+ 
+    // Perform rotation
+    r->left = y;
+    y->right = t;
+ 
+    // Update heights
+    y->height = max(height(y->left),   
+                    height(y->right)) + 1;
+    r->height = max(height(r->left),
+                    height(r->right)) + 1;
+ 
+    // Return new root
+    return r;
+}
+
+// Get Balance factor of node N
+int getBalance(Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+
+Node* insert(Node* node, int key)
+{
+    /* 1. Perform the normal BST insertion */
+    if (node == NULL)
+        return(newNode(key));
+ 
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else // Equal keys are not allowed in BST
+        return node;
+ 
+    /* 2. Update height of this ancestor node */
+    node->height = 1 + max(height(node->left),
+                        height(node->right));
+ 
+    /* 3. Get the balance factor of this ancestor
+        node to check whether this node became
+        unbalanced */
+    int balance = getBalance(node);
+ 
+    // If this node becomes unbalanced, then
+    // there are 4 cases
+ 
+    // Left Left Case
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+ 
+    // Right Right Case
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+ 
+    // Left Right Case
+    if (balance > 1 && key > node->left->key)
+    {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+ 
+    // Right Left Case
+    if (balance < -1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+ 
+    /* return the (unchanged) node pointer */
+    return node;
+}
+// A utility function to print preorder
+// traversal of the tree.
+// The function also prints height
+// of every node
+void preOrder(Node *root)
+{
+    if(root != NULL)
+    {
+        cout << root->key << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+ 
+// Driver Code
+int main()
+{
+    Node *root = NULL;
+     
+    /* Constructing tree given in
+    the above figure */
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
+    cout << "Preorder traversal of the "
+            "constructed AVL tree is \n";
+    preOrder(root);
+     
+    return 0;
+}
